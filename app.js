9adad6b8d26ea13af9901533e55ee6f946721f52
Czeko-baton <67105044+Czeko-baton@users.noreply.github.com>
2021-06-27 20:50:29 +0100

    added intro slider

--- app.js
+++ app.js
@@ -158,3 +158,12 @@ cardHolder.addEventListener('click', (e) => {
 // set date ----------------------------------------------------------------------------------
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
+// intro --------------------------------------------------------------------------------------
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+
+tl.to('.textInIntro', { y: '0%', duration: 1, stagger: 0.25 });
+tl.to('.intro-slider', { y: '-100%', duration: 1.5, delay: 0.5 });
+tl.to('.intro', { y: '-100%', duration: 1 }, '-=1.1');
+tl.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo('.caption', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
