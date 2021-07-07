6dd31f5864a860653bec5d724914766d00c16253
Czeko-baton <67105044+Czeko-baton@users.noreply.github.com>
2021-07-07 19:05:42 +0100

    asdddd

--- app.js
+++ app.js
@@ -167,10 +167,3 @@ tl.to('.intro-slider', { y: '-100%', duration: 1.5, delay: 0.5 });
 tl.to('.intro', { y: '-100%', duration: 1 }, '-=1.1');
 tl.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo('.caption', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
-
-// clear path
-// bit more of stuff
-// asdasddsadddd;
-// putting even more
-// sddddddddd;
-// xxxxxxx
