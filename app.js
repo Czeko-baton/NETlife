39cb54601c2abc0d341f5eab5fb5b66f43e6298f
Czeko-baton <67105044+Czeko-baton@users.noreply.github.com>
2021-09-10 21:59:08 +0100

    asdnsdaskdnaskdnksndksn

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ tl.fromTo('.caption', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
 // i understand, not much here
 // adding it
 // pus
-// it's the third
+// ano
 // yah yeah, stfu
 // i know but haey
 // at least one
